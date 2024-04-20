d2084bf96c8d49e3bec8ceabd97a7ac7e16bdc2c
ArminRezaei <arminrezaei53@gmail.com>
2024-04-20 23:14:31 +0330

    fix ui

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,14 +1,70 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
+import { defaultStyles } from '@/constants/Styles'
+import Colors from '@/constants/Colors'
+import { Link } from 'expo-router'
 
-export class signup extends Component {
-    render() {
-        return (
-            <View>
-                <Text>signup</Text>
-            </View>
-        )
+const signup = () => {
+    const [countryCode, setCountryCode] = useState("+49")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const onSignUp = async () => {
+
     }
+    return (
+    
+        <View style={defaultStyles.container}>
+            <Text style={defaultStyles.header}>Let's get started</Text>
+            <Text style={defaultStyles.descriptionText}>
+                Enter your phone number. We will send you a confirmation code there
+            </Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Country code"
+                    placeholderTextColor={Colors.gray}
+                    value={countryCode}
+                />
+                <TextInput
+                    style={[styles.input, { flex: 1 }]}
+                    placeholder="Mobile number"
+                    placeholderTextColor={Colors.gray}
+                    keyboardType="numeric"
+                    value={phoneNumber}
+                    onChangeText={setPhoneNumber}
+                />
+            </View>
+            <Link href={"/login"} replace asChild>
+                <TouchableOpacity>
+                    <Text style={defaultStyles.textLink}>
+                        Already have an account? Log in
+                    </Text>
+                </TouchableOpacity>
+            </Link>
+            <View style={{ flex: 1 }} />
+            <TouchableOpacity style={[defaultStyles.pillButton, { marginTop: 20, backgroundColor: phoneNumber !== "" ? styles.enabled.backgroundColor : styles.disabled.backgroundColor }]} onPress={onSignUp}>
+                <Text style={defaultStyles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
+        </View>
+ 
+    )
 }
-
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 40,
+        flexDirection: 'row',
+    },
+    input: {
+        backgroundColor: Colors.lightGray,
+        padding: 20,
+        borderRadius: 16,
+        fontSize: 20,
+        marginRight: 10,
+    },
+    enabled: {
+        backgroundColor: Colors.primary,
+    },
+    disabled: {
+        backgroundColor: Colors.primaryMuted,
+    },
+})
 export default signup
